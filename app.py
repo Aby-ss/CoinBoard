ea2c8e65b38a2f7dff3639114c0033de8764accc
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-07-24 23:34:25 +0400

    Updated Dashboard for COmpany overview ✅

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import re
 import requests
 import asciichartpy
 
@@ -31,7 +32,7 @@ class Footer(Placeholder):
     """
 
 
-class Taskwise(App):
+class CoinBoard(App):
     CSS_PATH = "layout.css"
 
     def compose(self) -> ComposeResult:
@@ -41,7 +42,7 @@ class Taskwise(App):
         function = 'TIME_SERIES_DAILY'
         output_size = 'compact'
 
-        params = {
+        chart_params = {
             'function': function,
             'symbol': "AAPL",
             'outputsize': output_size,
@@ -49,7 +50,7 @@ class Taskwise(App):
         }
 
         
-        response = requests.get(base_url, params=params)
+        response = requests.get(base_url, params=chart_params)
         data = response.json()
 
         if 'Error Message' in data:
@@ -66,20 +67,50 @@ class Taskwise(App):
             # Create and display ASCII chart
             chart = asciichartpy.plot(close_prices, {"width": 5, "height": 10, "format": "{:8.2f}"})
             # print(Panel(chart, title=f"Monthly Close Prices for {symbol}", border_style="bold white", box=box.SQUARE))
+            
+        company_overview_params = {
+        'function': "OVERVIEW",
+        'symbol': "AAPL",
+        'apikey': "78H5RH2BRNG4G5Z6"
+    }
+            
+        response = requests.get(base_url, params=company_overview_params)
+        data = response.json()
+
+        if 'Error Message' in data:
+            print(f"Error: {data['Error Message']}")
+        else:
+            overview_text = ""
+            for key, value in data.items():
+                overview_text += f"{key}: {value}\n"
+
+            # Extract important variables
+            description = re.search(r"Description: (.+)", overview_text).group(1)
+            sector = re.search(r"Sector: (.+)", overview_text).group(1)
+            industry = re.search(r"Industry: (.+)", overview_text).group(1)
+            name = re.search(r"Name: (.+)", overview_text).group(1)
+            exchange = re.search(r"Exchange: (.+)", overview_text).group(1)
+            currency = re.search(r"Currency: (.+)", overview_text).group(1)
+            address = re.search(r"Address: (.+)", overview_text).group(1)
+            asset_type = re.search(r"AssetType: (.+)", overview_text).group(1)
+            
+            company_overview = f"{name}\n{description}\n\nIndustry: {industry}\nSection: {sector}\nAddress: {address}\ncurrency: {currency}"
+            
 
             
-        column_text = f"{chart}"
+        chart_text = f"{chart}"
+        company_overview_text = f"{company_overview}"
         
         yield Header("CoinBoard", classes="Header",)
         yield Footer("Empowering Investments, Simplifying Decisions!")
         yield Horizontal(
             Vertical(
-                Static(f"{column_text}"),
+                Static(f"{chart_text}"),
                 Static("More Information"),
                 classes="column",
             ),
             Vertical(
-                Static("Company Information"),
+                Static(f"{company_overview_text}"),
                 classes="column",
             ),
         )
@@ -88,10 +119,34 @@ class Taskwise(App):
             self.Header.styles.background = "#9932CC"
 
 
+def get_company_overview(api_key, symbol):
+    base_url = 'https://www.alphavantage.co/query'
+    function = 'OVERVIEW'
 
+    params = {
+        'function': function,
+        'symbol': symbol,
+        'apikey': api_key
+    }
+
+    try:
+        response = requests.get(base_url, params=params)
+        data = response.json()
+
+        if 'Error Message' in data:
+            print(f"Error: {data['Error Message']}")
+        else:
+            overview_text = ""
+            for key, value in data.items():
+                overview_text += f"{key}: {value}\n"
+            
+            return overview_text
+                
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
 
 
     
 if __name__ == "__main__":
-    app = Taskwise()
+    app = CoinBoard()
     app.run()
